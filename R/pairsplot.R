efb6262adcc525ae142dfe136a68638962cb7593
Aaron Cochrane <56330575+akcochrane@users.noreply.github.com>
2025-07-31 13:59:29 -0400

    Update estimation of bayes factors from correlation coefficients
    Switch from BayesFactor::ttest.tstat() to BayesFactor::correlationBF

--- R/pairsplot.R
+++ R/pairsplot.R
@@ -1,11 +1,12 @@
 
 #' Create a \code{psych::pairs.panels()} plot with standard formatting
 #'
-#' The default title includes approximate Bayes Factors-based estimates of regions providing "At least moderate
+#' The default title includes \strong{approximate heuristic} Bayes Factors-based estimates of regions providing "At least moderate
 #' evidence for the lack of a correlation" or "at least moderate evidence for a correlation." The BF approximation
-#' uses the \code{BayesFactor::ttest.tstat()} function, and finds the r value associated with a T value associated with a
+#' uses the \code{ACmisc::r2bf} function, and finds the r value associated with a
 #' raw BF <.33333 or >3. \strong{Beware}: The \emph{n} used in these calculations is the number of rows of the data frame.
-#' If you have missing data, these estimates could be wildly off.
+#' If you have missing data, these estimates could be wildly off. To speed up the use of the function,
+#' supply a title to \code{main} to prevent the estimation of the Bayes Factors.
 #'
 #' @param d Data frame to be plotted
 #' @param method Correlation method to be used: product-moment ('pearson') or rank ('spearman') ?
@@ -21,37 +22,29 @@
 #' pairsplot(d)
 #' 
 pairsplot <- function(d, method = 'Spearman', main = '',stars=T,color='darkgrey'){
-
+  
   library(psych)
-
+  
   par(family='serif')
-
+  
   if(nchar(main)==0){
-
-    alt_thresh <- suppressMessages({suppressWarnings({
-      optimise(
-      function(r_val){
-        (3-BayesFactor::ttest.tstat(psych::r2t(r_val,nrow(d)),nrow(d),simple = T) )^2
-      },c(.000001,.99999))$minimum
-  })})
-
-    null_thresh <- suppressMessages({suppressWarnings({
-    optimise(
-      function(r_val){
-        (.333333333333-BayesFactor::ttest.tstat(psych::r2t(r_val,nrow(d)),nrow(d),simple = T) )^2
-      },c(.000001,.99999))$minimum
-    })})
-
-    main = paste0('Pairs Plot, ',method,'\nApproximate null acceptance region |r|<',round(null_thresh,2),'; null rejection region |r|>',round(alt_thresh,2))
-    }
-
+    
+    
+    bf_from_r_n <- r2bf(r = .5, n = nrow(d), returnThresholds = T)
+    alt_thresh <- attr(bf_from_r_n, 'results')['alternThresh']
+    
+    null_thresh <- attr(bf_from_r_n, 'results')['nullThresh']
+    
+    main = paste0('Pairs Plot, ',method,'\nApproximate heuristic null acceptance region |r|<',round(null_thresh,2),'; null rejection region |r|>',round(alt_thresh,2))
+  }
+  
   for(curVar in colnames(d)){
     if(is.character(d[,curVar])){d[,curVar] <- NULL}
     if(is.factor(d[,curVar])){
       if(length(unique(d[,curVar])) > 2){d[,curVar] <- NULL}
     }
   }
-
+  
   pairs.panels(d,
                method = tolower(method),
                ci=T,
--- R/r2bf.R
+++ R/r2bf.R
@@ -1,52 +1,100 @@
 
 #' Convert correlation \emph{r} to a Bayes Factor
 #' 
-#' Uses default values from the \code{BayesFactor::ttest.tstat} function to convert an 
-#' effect size \emph{r} to a Bayes factor, given the sample size \emph{n}. Also includes as attributes the thresholds, 
+#' Uses default values from the \code{BayesFactor::correlationBF} function to convert an 
+#' effect size \emph{r} to a Bayes factor by simulating datasets, given the sample size \emph{n}. Can also include as attributes the thresholds, 
 #' at that sample size, for conventionally "accepting the null hypothesis" (i.e., BF < .333)
 #' or "rejecting the null hypothesis" (i.e., BF > 3). Bayes Factors are returned "raw," that is,
 #' as the ratio of the evidence for the alternative hypothesis to the evidence for the null hypothesis.
-#'
+#' All values returned should be treated as \strong{approximate}, and the hypothesis 
+#' acceptance thresholds should be treated as \strong{heuristics}. For Bayes Factors for
+#' a particular correlation, using \code{\link[BayesFactor]{correlationBF}} is \strong{highly recommended}.
+#' 
 #' @param r 
 #' @param n 
+#' @param rscale Passes to the \code{rscale} argument of \code{\link[BayesFactor]{correlationBF}}. Choose from "medium.narrow", "medium", "wide", and "ultrawide". As in \code{correlationBF}, defaults to "medium".
+#' @param nsims Number of simulated datasets for which to find the Bayes Factors; more simulations will lead to more-stable results
+#' @param returnThresholds Logical. Should the boundaries indicating "decision thresholds" be estimated?
 #'
 #' @export
-#'
+#' 
 #' @examples
 #' exampleR <- cor(dat_cochraneEtAl_2019_PLOSOne$enum , dat_cochraneEtAl_2019_PLOSOne$mot)
 #' r2bf(exampleR , nrow(dat_cochraneEtAl_2019_PLOSOne))
 #' 
-r2bf <- function(r,n){
+#' 
+#' 
+r2bf <- function(r,n,rscale = 'medium',nsims = 20, returnThresholds = FALSE){
   
-  getBF <- function(r){
-    BayesFactor::ttest.tstat(psych::r2t(r, 
-                                        n), n, simple = T)
+  require(BayesFactor)
+  require(mvtnorm)
+  
+  getBF <- function(rIn, nSimsIn = nsims){
+    
+    ## previously this method, but it seems to take a shortcut (but maybe gives the same result in the end..?)
+    # BayesFactor::ttest.tstat(psych::r2t(r,n), n, simple = T)
+    
+    full_bf <- c()
+    for(. in 1:nSimsIn){
+      
+      correlDat <- list()
+      for(curDat in 1:20){
+        correlDat[[curDat]] <- 
+          rmvnorm(n,sigma = matrix(c(1,rIn,rIn,1),nrow = 2) )
+      }
+      
+      correlDat <- correlDat[[which.min(abs(unlist(
+        lapply(correlDat,function(x){cor(x)[1,2]})
+      )))]]
+      
+      full_bf <- c(full_bf
+                   ,correlationBF(correlDat[,1],correlDat[,2])@bayesFactor$bf)
+      
+    }
+    exp(mean(full_bf))
   }
   
- bf <-  getBF(r=r)
- 
- attr(bf,'results') <- c(n=as.integer(n),r=r,bf = bf)
- 
- attr(bf,'results')['nullThresh'] <- suppressMessages({
-    suppressWarnings({
-      optimise(function(r_val) {
-        (0.333333333333 - getBF(r_val))^2
-      }, c(1e-06, 0.99999))$minimum
-    })
-  })
- 
- attr(bf,'results')['alternThresh'] <- suppressMessages({
-   suppressWarnings({
-     optimise(function(r_val) {
-       (3- getBF(r_val))^2
-     }, c(1e-06, 0.99999))$minimum
-   })
- })
-
-   
+  bf <- getBF(rIn=r)
   
- 
- bf
+  attr(bf,'results') <- c(n=as.integer(n),r=r,bf = bf)
+  
+  if(returnThresholds){
+    
+    nullThresh <- c()
+    alternThresh <- c()
+    
+    for(. in 1:25){
+      
+      nullThresh <- c(nullThresh
+                      ,suppressMessages({
+                        suppressWarnings({
+                          optimize(function(r_val,nsims) {
+                            (0.333333333333 - getBF(rIn = r_val,nSimsIn = nsims))^2
+                          }, c(1e-06, 0.99999)
+                          ,nsims = nsims)$minimum
+                        })
+                      })
+      )
+      
+      alternThresh <- c(alternThresh
+                        ,suppressMessages({
+                          suppressWarnings({
+                            optimize(function(r_val,nsims) {
+                              (3- getBF(rIn = r_val,nSimsIn = nsims))^2
+                            }, c(1e-06, 0.99999)
+                            ,nsims = nsims)$minimum
+                          })
+                        })
+      )
+      
+    }
+    
+    attr(bf,'results')['nullThresh'] <- median(nullThresh)
+    attr(bf,'results')['alternThresh'] <- median(alternThresh)
+    
+  }
+  
+  bf
   
 }
 
