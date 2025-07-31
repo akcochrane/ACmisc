b259338ac82703bf6bd4426e04570f2717ba6700
Aaron Cochrane <56330575+akcochrane@users.noreply.github.com>
2025-07-31 15:26:49 -0400

    Alter estimation of BF from R
    Refinement of r2bf and pairsplot functions.

--- R/pairsplot.R
+++ R/pairsplot.R
@@ -5,13 +5,18 @@
 #' evidence for the lack of a correlation" or "at least moderate evidence for a correlation." The BF approximation
 #' uses the \code{ACmisc::r2bf} function, and finds the r value associated with a
 #' raw BF <.33333 or >3. \strong{Beware}: The \emph{n} used in these calculations is the number of rows of the data frame.
-#' If you have missing data, these estimates could be wildly off. To speed up the use of the function,
+#' If you have missing data, these estimates could be wildly off. 
+#' 
+#' The Bayes Factor estimation takes some time. To speed up the use of the function,
 #' supply a title to \code{main} to prevent the estimation of the Bayes Factors.
+#' 
+#' It is \strong{strongly recommended} to use \code{\link[BayesFactor]{correlationBF}} directly
+#' for a Bayes Factor for any particular correlation of interest.
 #'
 #' @param d Data frame to be plotted
 #' @param method Correlation method to be used: product-moment ('pearson') or rank ('spearman') ?
 #' @param main Plot title, as in plot()
-#' @param stars Do you want magic stars? (same as \code{pairs.panels()})
+#' @param stars Do you want magic stars? (same as \code{\link[psych]{pairs.panels}})
 #' @param color What color do you want in the histogram bars?
 #'
 #' @export
--- R/r2bf.R
+++ R/r2bf.R
@@ -1,8 +1,9 @@
 
 #' Convert correlation \emph{r} to a Bayes Factor
 #' 
-#' Uses default values from the \code{BayesFactor::correlationBF} function to convert an 
-#' effect size \emph{r} to a Bayes factor by simulating datasets, given the sample size \emph{n}. Can also include as attributes the thresholds, 
+#' Uses default values from the \code{\link[BayesFactor]{ttest.tstat}} function to convert an 
+#' effect size \emph{r} to a Bayes factor by first finding the T value, given the sample size \emph{n}. 
+#' Can also include as attributes the thresholds, 
 #' at that sample size, for conventionally "accepting the null hypothesis" (i.e., BF < .333)
 #' or "rejecting the null hypothesis" (i.e., BF > 3). Bayes Factors are returned "raw," that is,
 #' as the ratio of the evidence for the alternative hypothesis to the evidence for the null hypothesis.
@@ -12,8 +13,8 @@
 #' 
 #' @param r 
 #' @param n 
-#' @param rscale Passes to the \code{rscale} argument of \code{\link[BayesFactor]{correlationBF}}. Choose from "medium.narrow", "medium", "wide", and "ultrawide". As in \code{correlationBF}, defaults to "medium".
-#' @param nsims Number of simulated datasets for which to find the Bayes Factors; more simulations will lead to more-stable results
+#' @param rscale Passes to the \code{rscale} argument of \code{\link[BayesFactor]{ttest.tstat}}. Choose from "medium.narrow", "medium", "wide", and "ultrawide". As in \code{ttest.tstat}, defaults to "medium".
+#' @param nsims Not currently used.
 #' @param returnThresholds Logical. Should the boundaries indicating "decision thresholds" be estimated?
 #'
 #' @export
@@ -31,27 +32,28 @@ r2bf <- function(r,n,rscale = 'medium',nsims = 20, returnThresholds = FALSE){
   
   getBF <- function(rIn, nSimsIn = nsims){
     
-    ## previously this method, but it seems to take a shortcut (but maybe gives the same result in the end..?)
-    # BayesFactor::ttest.tstat(psych::r2t(r,n), n, simple = T)
+    # this gives much more stable results than the below
+    BayesFactor::ttest.tstat(psych::r2t(rIn,n), n, simple = T, rscale = rscale)
     
-    full_bf <- c()
-    for(. in 1:nSimsIn){
-      
-      correlDat <- list()
-      for(curDat in 1:20){
-        correlDat[[curDat]] <- 
-          rmvnorm(n,sigma = matrix(c(1,rIn,rIn,1),nrow = 2) )
-      }
-      
-      correlDat <- correlDat[[which.min(abs(unlist(
-        lapply(correlDat,function(x){cor(x)[1,2]})
-      )))]]
-      
-      full_bf <- c(full_bf
-                   ,correlationBF(correlDat[,1],correlDat[,2])@bayesFactor$bf)
-      
-    }
-    exp(mean(full_bf))
+    ## Getting this to work well is not really happening (estimates for the thresholds are quite high r for both null and non-null inferences, when using default priors)
+    # full_bf <- c()
+    # for(. in 1:nSimsIn){
+    #   
+    #   correlDat <- list()
+    #   for(curDat in 1:20){
+    #     correlDat[[curDat]] <- 
+    #       rmvnorm(n,sigma = matrix(c(1,rIn,rIn,1),nrow = 2) )
+    #   }
+    #   
+    #   correlDat <- correlDat[[which.min(abs(unlist(
+    #     lapply(correlDat,function(x){cor(x)[1,2]})
+    #   )))]]
+    #   
+    #   full_bf <- c(full_bf
+    #                ,correlationBF(correlDat[,1],correlDat[,2])@bayesFactor$bf)
+    #   
+    # }
+    # exp(mean(full_bf))
   }
   
   bf <- getBF(rIn=r)
@@ -63,15 +65,16 @@ r2bf <- function(r,n,rscale = 'medium',nsims = 20, returnThresholds = FALSE){
     nullThresh <- c()
     alternThresh <- c()
     
-    for(. in 1:25){
+    for(. in 1:3){
       
       nullThresh <- c(nullThresh
                       ,suppressMessages({
                         suppressWarnings({
-                          optimize(function(r_val,nsims) {
+                          optimize(function(r_val) {
                             (0.333333333333 - getBF(rIn = r_val,nSimsIn = nsims))^2
                           }, c(1e-06, 0.99999)
-                          ,nsims = nsims)$minimum
+                          # ,nsims = nsims
+                          )$minimum
                         })
                       })
       )
@@ -80,9 +83,12 @@ r2bf <- function(r,n,rscale = 'medium',nsims = 20, returnThresholds = FALSE){
                         ,suppressMessages({
                           suppressWarnings({
                             optimize(function(r_val,nsims) {
-                              (3- getBF(rIn = r_val,nSimsIn = nsims))^2
+                              (3- getBF(rIn = r_val
+                                        # ,nSimsIn = nsims
+                                        ))^2
                             }, c(1e-06, 0.99999)
-                            ,nsims = nsims)$minimum
+                            # ,nsims = nsims
+                            )$minimum
                           })
                         })
       )
